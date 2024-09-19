7ce76945f4000ec54978e0db172353961b1d60ea
fajila3257 <umeshaik576@gmail.com>
2024-09-19 01:23:15 -0700

    Create cricket team.py

---
+++ cricket team.py
@@ -0,0 +1,21 @@
+It was in the 1997-98 season that Ranchi saw the rise of the Captain Cool in the interschool trophy between DAV Jawahar Vidhya Mandir and Kendriya School. It was in that match Dhoni convinced Banerjee to be the opener and justified it with a double century.
+Write a program to display the details of the match with Team name, Scores of the team and Overs played.
+ans::
+
+a=input()
+print('team',a)
+b=input()
+print('Team Name',b)
+c=input()
+print('Score:'+c)
+d=input()
+print('Overs Played',d)
+OUTPUT:
+1
+team 1
+Dav jawahar vidhya mandhir
+Team Name Dav jawahar vidhya mandhir
+150
+Score:150
+20
+Overs Played 20
